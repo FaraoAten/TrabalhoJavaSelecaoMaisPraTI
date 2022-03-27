a2476a8fea7f76015603740d706c7d88c274fd53
FaraoAten <atenfernandes@gmail.com>
2022-03-27 19:56:05 -0300

    Primeira função concluída (90%) e início da secunda função

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,4 @@
-//to git https://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@github.com/FaraoAten/TrabalhoJavaSelecaoMaisPraTI.git
+//to git https://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@github.com/FaraoAten/TrabalhoJavaSelecaoMaisPraTI.git
 
 package main;
 
