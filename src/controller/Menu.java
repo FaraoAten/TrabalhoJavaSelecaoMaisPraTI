f412e307002fd46eecbc78faef9e29198f060a41
FaraoAten <atenfernandes@gmail.com>
2022-04-01 17:31:21 -0300

    Criação dos métodos de validação de inputs para deixar o código mais limpo.

--- src/controller/Menu.java
+++ src/controller/Menu.java
@@ -11,51 +11,28 @@ import model.Pessoa;
 
 public class Menu {
 
-    Scanner entrada = new Scanner(System.in,"ISO-8859-1");
+    Scanner entrada = new Scanner(System.in, "ISO-8859-1");
     private List<Pessoa> listaDeCadastrados = new ArrayList();
-    private String regexNome = "[a-zA-Zà-üÀ-Ü' ]{3,}";
-    private String regexTelefone = "[0-9]{10,12}";
-    private String regexNotaFinal = "[0-9]{1,}[\\.][0-9]{1,}|[0-9]{1,}";
 
     public void cadastrarPessoaOuAluno() {
         System.out.println("\nDigite o nome da pessoa ou aluno a ser cadastrado:");
-        String nome = entrada.nextLine();
-        while (!nome.matches(regexNome)) {
-            System.out.println("\nPor favor digite um nome:");
-            nome = entrada.nextLine();
-        }
-
+        String nome = validarNome(entrada.nextLine());
+        
         System.out.println("\nDigite o DDD+número telefonico da pessoa ou aluno a ser cadastrado (apenas os números):");
-        String telefone = entrada.nextLine();
-        while (!telefone.matches(regexTelefone)) {
-            System.out.println("\nPor favor digite um número telefonico válido:\n(No formato DDD+número e colocando apenas os números)");
-            telefone = entrada.nextLine();
-        }
+        String telefone = validarTelefone(entrada.nextLine());
 
         System.out.println("\nDigite a data de nascimento da pessoa ou aluno a ser cadastrado (no padrão dd/mm/aaaa):");
-        LocalDate nascimento = LocalDate.now();
-        while (true) {
-            try {
-                nascimento = LocalDate.parse(entrada.nextLine(), DateTimeFormatter.ofPattern("dd/MM/uuuu"));
-                break;
-            } catch (DateTimeParseException e) {
-                System.out.println("\nPor favor digite uma data válida:\n(No formato dd/mm/aaaa)");
-            }
-        }
+        String nascimento = validarNascimento(entrada.nextLine());
 
         System.out.println("\nCaso queira cadastrar um aluno, digite a nota final do curso:\n(Para cadastrar uma pessoa clique na tecla 'Enter' deixando esse campo em branco)");
         String notaFinal = entrada.nextLine();
 
         if (notaFinal.isBlank()) {
-            listaDeCadastrados.add(new Pessoa(nome, telefone, nascimento));
+            listaDeCadastrados.add(new Pessoa(nome, telefone, LocalDate.parse(nascimento, DateTimeFormatter.ofPattern("dd/MM/uuuu"))));
             System.out.println("Pessoa cadastrada com sucesso.\n");
         } else {
-            while (!notaFinal.matches(regexNotaFinal)) {
-                System.out.println("\nPor favor digite uma nota válida:\n(Para números não-inteiros utilize ponto ao invés de vírgula)");
-                notaFinal = entrada.nextLine();
-            }
-
-            listaDeCadastrados.add(new Aluno(nome, telefone, nascimento, Double.parseDouble(notaFinal)));
+            notaFinal = validarNotaFinal(notaFinal);
+            listaDeCadastrados.add(new Aluno(nome, telefone, LocalDate.parse(nascimento, DateTimeFormatter.ofPattern("dd/MM/uuuu")), Double.parseDouble(notaFinal)));
             System.out.println("\nAluno cadastrado com sucesso.\n");
         }
     }
@@ -80,53 +57,28 @@ public class Menu {
         listarTodosCadastrados();
         if (listaDeCadastrados.size() > 0) {
             System.out.println("Digite o número da posição de quem você quer editar os dados:");
-            int i = 1;
-            while (true) {
-                try {
-                    i = Integer.parseInt(entrada.nextLine());
-                    listaDeCadastrados.get(i - 1);
-                    break;
-                } catch (NumberFormatException e) {
-                    System.out.println("Valor inválido\n\nPor favor digite o número de uma posição:");
-                } catch (IndexOutOfBoundsException e) {
-                    System.out.println("Posição inexistente\n\nPor favor digite uma posição válida:");
-                }
-            }
+            int i = validarPosicao(entrada.nextLine());
 
             System.out.println("\nCaso não queira atualizar algum dado clique na tecla 'Enter' deixando o campo em branco.");
 
             System.out.println("\nDigite o novo nome:");
             String nome = entrada.nextLine();
             if (!nome.isBlank()) {
-                while (!nome.matches(regexNome)) {
-                    System.out.println("\nPor favor digite um nome:");
-                    nome = entrada.nextLine();
-                }
+                nome = validarNome(nome);
                 listaDeCadastrados.get(i - 1).setNome(nome);
             }
 
             System.out.println("\nDigite o número número telefonico (apenas os números):");
             String telefone = entrada.nextLine();
             if (!telefone.isBlank()) {
-                while (!telefone.matches(regexTelefone)) {
-                    System.out.println("\nPor favor digite um número telefonico válido:\n(No formato DDD+número e colocando apenas os números)");
-                    telefone = entrada.nextLine();
-                }
+                telefone = validarTelefone(telefone);
                 listaDeCadastrados.get(i - 1).setTelefone(telefone);
             }
 
             System.out.println("\nDigite a nova data de nascimento (no padrão dd/mm/aaaa):");
             String nascimento = entrada.nextLine();
             if (!nascimento.isBlank()) {
-                while (true) {
-                    try {
-                        LocalDate.parse(nascimento, DateTimeFormatter.ofPattern("dd/MM/uuuu"));
-                        break;
-                    } catch (DateTimeParseException e) {
-                        System.out.println("\nPor favor digite uma data válida:\n(No formato dd/mm/aaaa)");
-                        nascimento = entrada.nextLine();
-                    }
-                }
+                nascimento = validarNascimento(nascimento);
                 listaDeCadastrados.get(i - 1).setNascimento(LocalDate.parse(nascimento, DateTimeFormatter.ofPattern("dd/MM/uuuu")));
             }
 
@@ -134,10 +86,7 @@ public class Menu {
                 System.out.println("\nDigite a nova nota final do curso:");
                 String notaFinal = entrada.nextLine();
                 if (!notaFinal.isBlank()) {
-                    while (!notaFinal.matches(regexNotaFinal)) {
-                        System.out.println("\nPor favor digite uma nota válida:\n(Para números não-inteiros utilize ponto ao invés de vírgula)");
-                        notaFinal = entrada.nextLine();
-                    }
+                    notaFinal = validarNotaFinal(notaFinal);
                     Aluno.parseAluno(listaDeCadastrados.get(i - 1)).setNotaFinal(Double.parseDouble(notaFinal));
                 }
             }
@@ -152,18 +101,7 @@ public class Menu {
         listarTodosCadastrados();
         if (listaDeCadastrados.size() > 0) {
             System.out.println("Digite o número da posição de quem você quer deletar os dados:");
-            int i = 1;
-            while (true) {
-                try {
-                    i = Integer.parseInt(entrada.nextLine());
-                    listaDeCadastrados.get(i - 1);
-                    break;
-                } catch (NumberFormatException e) {
-                    System.out.println("Valor inválido\n\nPor favor digite o número de uma posição:");
-                } catch (IndexOutOfBoundsException e) {
-                    System.out.println("Posição inexistente\n\nPor favor digite uma posição válida:");
-                }
-            }
+            int i = validarPosicao(entrada.nextLine());
 
             listaDeCadastrados.remove(i - 1);
 
@@ -186,5 +124,61 @@ public class Menu {
                     break;
             }
         }
+    }
+
+    private String validarNome(String nome) {
+        String regexNome = "[a-zA-Zà-üÀ-Ü' ]{3,}";
+        while (!nome.matches(regexNome)) {
+            System.out.println("\nPor favor digite um nome:");
+            nome = entrada.nextLine();
+        }
+        return nome;
+    }
+
+    private String validarTelefone(String telefone) {
+        String regexTelefone = "[0-9]{10,12}";
+        while (!telefone.matches(regexTelefone)) {
+            System.out.println("\nPor favor digite um número telefonico válido:\n(No formato DDD+número e colocando apenas os números)");
+            telefone = entrada.nextLine();
+        }
+        return telefone;
+    }
+
+    private String validarNascimento(String nascimento) {
+        while (true) {
+            try {
+                LocalDate.parse(nascimento, DateTimeFormatter.ofPattern("dd/MM/uuuu"));
+                break;
+            } catch (DateTimeParseException e) {
+                System.out.println("\nPor favor digite uma data válida:\n(No formato dd/mm/aaaa)");
+                nascimento = entrada.nextLine();
+            }
+        }
+        return nascimento;
+    }
+
+    private String validarNotaFinal(String notaFinal) {
+        String regexNotaFinal = "[0-9]{1,}[\\.][0-9]{1,}|[0-9]{1,}";
+        while (!notaFinal.matches(regexNotaFinal)) {
+            System.out.println("\nPor favor digite uma nota válida:\n(Para números não-inteiros utilize ponto ao invés de vírgula)");
+            notaFinal = entrada.nextLine();
+        }
+        return notaFinal;
+    }
+
+    private Integer validarPosicao(String posicao) {
+        int i = 1;
+        while (true) {
+            try {
+                i = Integer.parseInt(posicao);
+                listaDeCadastrados.get(i - 1);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Valor inválido\n\nPor favor digite o número de uma posição:");
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Posição inexistente\n\nPor favor digite uma posição válida:");
+            }
+        }
+        return i;
     }
 }
